8f03fd974e00d68b83da494e18f095acc73b6161
CWebber <116667571+KoderMx1@users.noreply.github.com>
2025-05-01 12:03:33 -0700

    wifi test

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-import time
+import subprocess
 
-start_time = time.monotonic()
+def get_wifi_info():
+	process = subprocess.Popen(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport','-I'], stdout=subprocess.PIPE)
+	out, err = process.communicate()
+	process.wait()
 
-while True:
-    current_time = time.monotonic()
-    elapsed_time = current_time - start_time
+	wifi_info = {}
+	for line in out.decode("utf-8").split("\n"):
+		if ": " in line:
+			key, val = line.split(": ")
+			key = key.replace(" ", "")
+			val = val.strip()
 
-    if elapsed_time >= 5:
-        # Code to be executed after 10 seconds goes here
-        print("This code ran after 10 seconds.")
-        break
-    # You can add other tasks or checks here if needed
+			wifi_info[key] = val
+
+	return wifi_info
+
+wifi_info = get_wifi_info()
+print(wifi_info["SSID"])
